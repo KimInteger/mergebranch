bf85992c64556c55806ba7f7fd97c42f1f1ff27d
KimInteger <kjs20151240@gmail.com>
2024-05-29 12:27:34 +0900

    서버method기준으로 조건문 생성

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const http = require('node:http');
 
 const fs = require('node:fs');
+
+const server = http.createServer((req,res)=>{
+  if(req.method === 'GET'){
+
+  } else if (req.method === 'POST') {
+
+  } else {
+
+  }
+});
+
+server.listen(3000);
